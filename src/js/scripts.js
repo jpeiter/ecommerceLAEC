601ee49025cae08b6414e478593d29439e262121
Jean Felipe Peiter <jpeiter15@gmail.com>
2018-10-30 16:05:52 -0300

    Página de Produto

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,25 @@
-$('.foto-menor-li').mouseover(function () {
-    var imgSrc = $(this).children().attr('src');
-    $('#foto-grande').attr('src', imgSrc.replace("-mini", ""));
-    $(this).addClass('border-top-brown');
-}).mouseout(function () {
-    $(this).removeClass('border-top-brown');
+
+
+$(document).ready(function () {
+    $("#cep", e).mask("00000-000")
 });
 
+function mudaFotoHover() {
+    $('.foto-menor-li').mouseover(function () {
+        var imgSrc = $(this).children().attr('src');
+        $('#foto-grande').attr('src', imgSrc.replace("-mini", ""));
+        $(this).addClass('border-top-brown');
+    }).mouseout(function () {
+        $(this).removeClass('border-top-brown');
+    });
+}
 
+function favoriteClick() {
+    $('#fav').click(function () {
+        $(this).toggleClass('fa-heart-o fa-heart');
+        $(this).toggleClass('yellow red');
+    });
+}
+
+mudaFotoHover();
+favoriteClick();
